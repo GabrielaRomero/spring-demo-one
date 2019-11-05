31fb80db79451b9696842cd3acdb777dbf58a804
cgramirezr <cgramirezr@indracompany.com>
2019-11-05 10:59:41 -0600

    nuevos beans

---
+++ src/com/gabriela/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,27 @@
+package com.gabriela.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.gabriela.springdemo.coachs.Coach;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		//Checar si son iguales
+		boolean result = (theCoach == alphaCoach);
+		
+		System.out.println("Apuntando  a los mismo objetos "+result);
+		System.out.println("Ubicacion de memoria para theCoach "+theCoach);
+		System.out.println("Ubicacion de memoria para alphaCoach "+alphaCoach);
+		
+		
+		context.close();
+	}
+
+}
--- src/com/gabriela/springdemo/service/HappyFortuneService.java
+++ src/com/gabriela/springdemo/service/HappyFortuneService.java
@@ -1,10 +1,19 @@
 package com.gabriela.springdemo.service;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+	
+	private String[] arrayFortunas = {"Buena suerte", "tu puedes hacerlo", "Eres el mejor"};
+	
+	private Random random = new Random();
 
 	@Override
 	public String getFortune() {
-		return "Hoy es tu dia de suerte";
+		int seleccionado = random.nextInt(arrayFortunas.length);
+		String fortuna = arrayFortunas[seleccionado];
+		
+		return  fortuna;
 	}
 
 }
